b50b61f9e223fa107fab59efd2eb100790523ef0
Parham Kouloubandi <122618163+datenlabor01@users.noreply.github.com>
2023-02-15 20:06:09 +0100

    Update Klimadashboard.py

--- Klimadashboard.py
+++ Klimadashboard.py
@@ -8,19 +8,6 @@ dat = pd.read_csv('data.csv')
 #Set stylesheet:
 app = Dash(external_stylesheets = [dbc.themes.LUX])
 
-#Set-up static graphs:
-#Bar for sectors:
-datBar = dat.groupby(["Ressort", "Jahr", "TypeOfSupport", "FinancialInstrument"])[["Klimafinanzierung in USD"]].sum()
-datBar = datBar.reset_index()
-#Bar for Bi-/Multi:
-datBarMulti = dat.groupby(["Jahr", "Channel"])[["Klimafinanzierung in USD"]].sum()
-datBarMulti = datBarMulti.reset_index()
-figMultiBar = px.bar(datBarMulti, x="Jahr", y="Klimafinanzierung in USD", color="Channel", barmode="group")
-#Bar for Ressort:
-datBarRessort = dat.groupby(["Jahr", "Ressort"])[["Klimafinanzierung in USD"]].sum()
-datBarRessort = datBarRessort.reset_index()
-figRessortBar = px.bar(datBarRessort, x="Jahr", y="Klimafinanzierung in USD", color="Ressort")
-
 #Set-up dynamic elements:
 #Slider Type of Support:
 slider = dcc.Slider(0, 3, step=None,
@@ -28,14 +15,14 @@ slider = dcc.Slider(0, 3, step=None,
         0: "Total",
         1: 'adaptation',
         2: 'mitigation',
-        3: 'cross-cutting'},
+        3: 'crosscutting'}, included=False,
     value= 0)
 
 slider_txt = ["Total", "adaptation", "mitigation", "cross-cutting"]
 
 #Dropdown for Year:
-year_dropdown_map = dcc.Dropdown(options=["All", 2021, 2022],
-                             value='All', style={"textAlign": "center"}, clearable=False, placeholder='Alle Jahre')
+year_dropdown = dcc.Dropdown(options=dat["Jahr"].unique(),
+                             value='All', style={"textAlign": "center"}, clearable=False, multi=True, placeholder='Alle Jahre')
 
 #Dropdown for Country:
 country_dropdown = dcc.Dropdown(options=dat['Recipient'].unique(),
@@ -50,39 +37,33 @@ app.layout = dbc.Container([
       #Dropdown for year:
       dbc.Row([
          dbc.Col([
-         html.Br(), year_dropdown_map, html.Br(),
+         html.Br(), year_dropdown,
          ], width = 6),
          ], justify = "center"),
       #Slider for type of support:
       dbc.Row([
          dbc.Col([
-         slider, html.Br(),
+         html.Br(),
+         dcc.Dropdown(id = "ressort", style={"textAlign": "center"}, clearable=False, multi=True, placeholder='Alle Ressorts'),
+         html.Br(), slider,
          ],  width = 6),
       ], justify = 'center'),
-      #Worldmap and Top-5 bar-chart in same row: 
       dbc.Row([
          dbc.Col([
             dcc.Graph(id = "map"),
+         ]),
+         dbc.Col([
+            dcc.Graph(id = "treemap"),
+         ]),
+      ]),
+      dbc.Row([
+         dbc.Col([
+            dcc.Graph(id = "BarBiMulti"),
             ]),
          dbc.Col([
             dcc.Graph(id = "BarTop5"),
          ]),
       ]), 
-      #Bar-chart for finance type in single row:
-      dbc.Row([
-         html.P('Nach Finanztyp:'), dcc.Graph(id = "bar"),
-      ]),
-      #two static graphs in same row:
-      dbc.Row([
-         dbc.Col([
-            html.P('Nach Bi-/Multilateral:'),
-            dcc.Graph(figure = figMultiBar, id = "barMulti"),
-         ]),
-         dbc.Col([
-            html.P('Nach Ressort:'),
-            dcc.Graph(figure = figRessortBar, id = "barRessort"),
-         ])
-      ]),
       #Dropdown for country:
       dbc.Row([
          dbc.Col([
@@ -117,49 +98,90 @@ app.layout = dbc.Container([
       dbc.Row([
          my_table := dash_table.DataTable(
          dat.to_dict('records'), [{"name": i, "id": i} for i in dat.columns],
-         filter_action="native", sort_action="native", page_size= 25, style_cell={'textAlign': 'left'},
-         style_header={'backgroundColor': 'rgb(210, 210, 210)', 'color': 'black', 'fontWeight': 'bold'})
+         filter_action="native", sort_action="native", page_size= 25,
+         style_cell={'textAlign': 'left', "whiteSpace": "normal", "height": "auto"},
+         style_header={'backgroundColor': 'rgb(11, 148, 153)', 'color': 'black', 'fontWeight': 'bold'},
+             style_data_conditional=[{
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(235, 240, 240)',
+        }], export_format= "xlsx"),
          ]),
     ])
 
-#First callback-function for graphs only dependent on year-dropdown and slider:
+#Funktion um nur Länder anzuzeigen, die in gewählter Kategorie sind:
 @app.callback(
-    [Output('map', 'figure'), Output("BarTop5", "figure"), Output('bar', 'figure')],
-    [Input(year_dropdown_map, 'value'), Input(slider, "value")]
+    Output("ressort", "options"),
+    [Input(year_dropdown, 'value'), Input(slider, 'value')],
 )
 
-def update_graph_1(selected_year_map, slider):
-   #Filter data based on cases for user input: 
-   if (selected_year_map == "All") & (slider == 0):
-      dat_map = dat
-      dat_bar = datBar.groupby(["Ressort", "FinancialInstrument"])[["Klimafinanzierung in USD"]].sum()
-      dat_bar = dat_bar.reset_index()
-   if (selected_year_map == "All") & (slider != 0):
-      dat_map = dat[dat["TypeOfSupport"] == slider_txt[slider]]
-      dat_bar = datBar.groupby(["Ressort", "TypeOfSupport", "FinancialInstrument"])[["Klimafinanzierung in USD"]].sum()
-      dat_bar = dat_bar.reset_index()
-      dat_bar = dat_bar[dat_bar["TypeOfSupport"] == slider_txt[slider]]
-   if (selected_year_map != "All") & (slider == 0):
-      dat_map = dat[dat["Jahr"] == selected_year_map]
-      dat_bar = datBar[datBar["Jahr"] == selected_year_map]
-   if (selected_year_map != "All") & (slider != 0):
-      dat_map = dat[(dat["Jahr"] == selected_year_map) & (dat["TypeOfSupport"] == slider_txt[slider])]
-      dat_bar = datBar[(datBar["Jahr"] == selected_year_map) & (datBar["TypeOfSupport"] == slider_txt[slider])]
+def support_options(selected_year, slider):
+   if (selected_year == "All") | (selected_year == []):
+      dat_temp = dat
+   else:
+      dat_temp = dat[dat["Jahr"].isin(selected_year)]
+   if slider == 0:
+      dat_temp = dat_temp
+   else:
+      dat_temp = dat_temp[dat_temp["TypeOfSupport"] == slider_txt[slider]]
+   return sorted(dat_temp['Ressort'].unique())
+   
+#First callback-function for graphs only dependent on year-dropdown and slider:
+@app.callback(
+    [Output('map', 'figure'), Output('treemap', 'figure'), Output("BarTop5", "figure"), Output("BarBiMulti", "figure")],
+    [Input(year_dropdown, 'value'), Input(slider, 'value'), Input("ressort", "value")]
+)
+
+def update_graph_1(selected_year, slider, ressort_dropdown):
+   #Filter data based on user input for year and slider:
+   if (selected_year == "All") | (selected_year == []):
+      dat_fil = dat
+   else:
+      dat_fil = dat[dat["Jahr"].isin(selected_year)]
+   if slider == 0:
+      dat_fil = dat_fil
+   else:
+      dat_fil = dat_fil[dat_fil["TypeOfSupport"] == slider_txt[slider]]
+   if (ressort_dropdown == []) | (ressort_dropdown == None):
+      dat_fil = dat_fil
+   else:
+      dat_fil = dat_fil[dat_fil["Ressort"].isin(ressort_dropdown)]
+   
+   if dat_fil.empty:
+      dat_fil = dat
+   
    #Prepare data for map and display it:
-   dat_map = dat_map.groupby(["Recipient"])[["Klimafinanzierung in USD"]].sum()
+   dat_map = dat_fil.groupby(["Recipient"])[["Klimafinanzierung in USD"]].sum()
    dat_map = dat_map.reset_index()
-
    figMap = px.choropleth(dat_map, locations ="Recipient", locationmode="country names", 
-   color_continuous_scale="Viridis", color="Klimafinanzierung in USD", range_color=(min(dat["Klimafinanzierung in USD"]), max(dat["Klimafinanzierung in USD"]*0.05)))
-   figMap.update_layout(coloraxis_showscale=False, title = "Nach Empfängerländern")
-   #Graph for Top-5 bar-chart 
-   figBarTop = px.bar(dat_map.nlargest(5, "Klimafinanzierung in USD"), x='Klimafinanzierung in USD', y='Recipient', orientation="h")
-   figBarTop.update_layout(yaxis={'title': ""}, xaxis={'title': ""}, title = "Top-5 Empfänger")
-   #Graph for bar chart:
-   figBar = px.bar(dat_bar, x='FinancialInstrument', y='Klimafinanzierung in USD', color="Ressort", hover_name = "Ressort")
-   figBar.update_layout(yaxis={'title': ""}, xaxis={'title': ""})
+   color_continuous_scale="Viridis", color="Klimafinanzierung in USD", range_color=(min(dat_map["Klimafinanzierung in USD"]), max(dat_map["Klimafinanzierung in USD"]*0.05)))
+   figMap.update_layout(coloraxis_showscale=False)
+   #Treemap:
+   figTree = px.treemap(dat_fil, path=[px.Constant("Total"), 'Kontinent', "Region"], values='Klimafinanzierung in USD', color='Kontinent')
+   #Graph for Top-5 bar-chart:
+   dat_bartop = dat_fil.groupby(["Recipient", "Region", "Channel"])[["Klimafinanzierung in USD"]].sum()
+   dat_bartop = dat_bartop.reset_index()
+   #Set data according to dropdown options:
+   #If Multilateral as only filter:
+   if ressort_dropdown == ["Multilateral"]:
+      dat_bartop = dat_bartop[dat_bartop["Channel"] == "multilateral"]   
+   if ressort_dropdown != None:
+      if "Multilateral" in ressort_dropdown:
+         dat_bartop = dat_bartop.groupby(["Recipient"])[["Klimafinanzierung in USD"]].sum()
+         dat_bartop = dat_bartop.reset_index()
+      #Discard multilateral recipients if "multilateral" not chosen as reporter:
+      else:
+         dat_bartop = dat_bartop[dat_bartop["Region"] != "Andere/nicht aufteilbar"]
+   else:
+      dat_bartop = dat_bartop[dat_bartop["Region"] != "Andere/nicht aufteilbar"]
 
-   return (figMap, figBarTop, figBar)
+   figBarTop = px.bar(dat_bartop.nlargest(5, "Klimafinanzierung in USD"), x='Klimafinanzierung in USD', y='Recipient', orientation="h")
+   figBarTop.update_layout(yaxis={'title': ""}, xaxis={'title': ""}, title = "Top-5 Empfänger")
+   #Bi-/Multi Bar-chart:
+   datBarMulti = dat_fil.groupby(["Jahr", "Channel"])[["Klimafinanzierung in USD"]].sum()
+   datBarMulti = datBarMulti.reset_index()
+   figMultiBar = px.bar(datBarMulti, x="Jahr", y="Klimafinanzierung in USD", color="Channel", barmode="group")
+
+   return (figMap, figTree, figBarTop, figMultiBar)
 
 #Second callback for country-dropdown:
 @app.callback(
